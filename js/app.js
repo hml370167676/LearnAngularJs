8082bb6b3d3911fe0e7b71761dfee5390601e1ca
minglu <370167676@qq.com>
2017-07-06 19:03:15 +0800

    update

---
+++ js/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by minglu on 2017/7/5.
+ */
+var app = angular.module('myApp',[]);
---
+++ js/controller/angularController.js
@@ -0,0 +1,9 @@
+/**
+ * Created by minglu on 2017/7/5.
+ */
+app.controller('InputTypeCheckBoxController',['$scope',function ($scope) {
+    $scope.checkboxModel = {
+        value1 : true,
+        value2 : 'YES'
+    };
+}]);
